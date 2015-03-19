18594d770c883be31046ffedd5155579e7d3d542
RenLikuan <renlikuan@gmail.com>
2015-03-19 22:59:17 +0800

    新建个性化二维码项目

---
+++ BQRCode/BQRCode.h
@@ -0,0 +1,32 @@
+
+// BQRCode.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CBQRCodeApp:
+// 有关此类的实现，请参阅 BQRCode.cpp
+//
+
+class CBQRCodeApp : public CWinApp
+{
+public:
+	CBQRCodeApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBQRCodeApp theApp;
